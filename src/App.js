b8d239a514d3a5e51d7d48f11b5a348bf9aa1ff1
Alexis Grisham <alexiskjg16@gmail.com>
2018-08-27 13:48:59 -0400

    snowman

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import WordBox from './Components/Textbox'
+import './App.css';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <WordBox/>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Textbox.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//import words from '../data/words.json'
+
+class WordBox extends Component {
+    render () {
+        console.log('thisisathing')
+        return (
+            <h1> SNOWMAN </h1>
+        )
+    }
+}
+
+export default WordBox;
